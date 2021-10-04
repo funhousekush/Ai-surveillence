09a94616c331f7b129019542f5d73c54b24eacdd
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-10-04 19:40:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+object = [];
 status = "";
 video = "";
 function preload()
@@ -14,16 +15,45 @@ function setup()
 function start()
 {
     objectdetection = ml5.objectDetector("cocossd", modelLoaded); 
-    document.getElementById("ststuss").innerHTML = "Status: Object detecting";
+    document.getElementById("statuss").innerHTML = "Status: Object detecting";
 }
 function modelLoaded()
 {
     console.log("Model has loaded");
     video.loop();
     video.speed(1);
-    video.volume(1);
+    video.volume(0);
+    status = true
 }
 function draw()
 {
-    image(video, 0, 0, 400, 300) 
+    image(video, 0, 0, 400, 300);
+    if (status != "")
+    {
+        objectdetection.detect(video, gotresults);
+        for(i=0; i<object.length ; i++)
+        {
+            document.getElementById("statuss").innerHTML = "Objects Detectd";
+            document.getElementById("detected").innerHTML = "Objects detected are :" + object.length;
+            fill(255, 0, 109);
+            noFill();
+            stroke(255,0,109);
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", object[i].x, object[i].y);
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+    }
+}
+
+function gotresults(error, results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    if (results)
+    {
+        console.log(results);
+        object = results;
+    }
 }
